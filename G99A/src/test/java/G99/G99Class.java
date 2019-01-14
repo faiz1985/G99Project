eadf7db632fccc2909b4698e152b047500179f2e
ffarooq <ffarooq@USB1JR0N2.metnet.net>
2019-01-14 14:05:25 -0500

    Further parameterization to run based on browser parameter

--- G99A/src/test/java/G99/G99Class.java
+++ G99A/src/test/java/G99/G99Class.java
@@ -3,25 +3,26 @@ package G99;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class G99Class {
 	WebDriver driver;
-	String fireFoxDriver, FFDriverPath, appURL, appUname, appPwd,verifyLoginTitle,invalidCredentialsError;
+	String BrowserDriver, FireFoxDriverPath, ChromeDriverPath, appURL, appUname, appPwd, verifyLoginTitle, invalidCredentialsError, successfulLoginMsg;
 	
 	@BeforeSuite
-	@Parameters({"FireFoxDriver", "driverPath", "AUTLink", "uname", "pwd"})
-	public void dataSource(String firefoxDriver, String DriverPath, String URL, String userName, String password) {
+	@Parameters({"browserDriver", "firefoxDriverPath","chromeDriverPath", "AUTLink", "uname", "pwd"})
+	public void dataSource(String browserDriver, String firefoxDriverPath, String chromeDriverPath, String URL, String userName, String password) {
 		//These are from TestNG.XML file
-		fireFoxDriver=firefoxDriver;
-		FFDriverPath=DriverPath;
+		BrowserDriver=browserDriver;
+		FireFoxDriverPath=firefoxDriverPath;
+		ChromeDriverPath=chromeDriverPath;
+		
 		appURL=URL;
 		appUname=userName;
 		appPwd=password;
@@ -29,19 +30,28 @@ public class G99Class {
 		//These are from Util.Java file
 		verifyLoginTitle=Util.verifyLoginTitle;
 		invalidCredentialsError=Util.invalidCredentialsError;
+		successfulLoginMsg=Util.successfulLoginMsg;
 	}
 	
 //@Parameters({"FireFoxDriver", "driverPath"})
   @BeforeClass()
   public void initialiseBrowser() {
-	  System.setProperty(fireFoxDriver, FFDriverPath);
-	  driver = new FirefoxDriver();
+	  
+	  if(BrowserDriver.contains("chrome")) {
+		  System.setProperty(BrowserDriver, ChromeDriverPath);
+		  driver = new ChromeDriver();
+	  }
+	  
+	  if(BrowserDriver.contains("gecko")) {
+		  System.setProperty(BrowserDriver, FireFoxDriverPath);
+	  	  driver = new FirefoxDriver();
+	  }
 	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //wait for a certain amount of time before throwing an exception that it cannot find the element on the page
   }
 	
 	//@Parameters({"AUTLink", "uname", "pwd"})
 	@Test
-	public void verifyLogin() throws InterruptedException {
+	public void verifyLogin() {
 		driver.get(appURL);
 		loginPageObjects LPO = new loginPageObjects(driver);
 		LPO.enterUname(appUname);
@@ -50,7 +60,7 @@ public class G99Class {
 		//Assert.assertEquals(driver.getTitle(), Util.verifyLoginTitle);		
 		
 		if(driver.getTitle().equals(verifyLoginTitle)) {
-			System.out.println("Login Successful");
+			System.out.println(successfulLoginMsg);
 		}
 	/*	else if(driver.switchTo().alert().getText().contains(invalidCredentialsError)) {
 			System.out.println("Invalid User ID or Password Entered");
--- G99A/src/test/java/G99/Util.java
+++ G99A/src/test/java/G99/Util.java
@@ -5,4 +5,5 @@ public class Util {
 	public static final String MY_NAME="Faisel Farooq";
 	public static final String verifyLoginTitle = "Guru99 Bank Manager HomePage";
 	public static final String invalidCredentialsError="User or Password is not valid";
+	public static final String successfulLoginMsg="Login Successful";
 }
